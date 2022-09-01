f5c98a5df880cb383d2dc9eb5a46f1d5a3e025f5
maraty-projects <maupamaupa497@gmail.com>
2022-09-01 18:02:23 +0200

    new commit

--- pages/projects/tetris.js
+++ pages/projects/tetris.js
@@ -1,5 +1,5 @@
 import {Container, Badge, Link, ListItem, List, Heading} from '@chakra-ui/react'
-import {Title, ProjectImage, Meta} from '../../components/Project'
+import {Heading, ProjectImage, Meta} from '../../components/Project'
 import P from '../../components/Paragraph'
 import Layout from '../../components/layouts/Article'
 
